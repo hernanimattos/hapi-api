a3b36f2c7177e0a8fd2be6df050c33dea9efb739
hernani <hernani.luiz@gmail.com>
2018-01-01 10:56:00 -0200

    Add seed for users

--- api/seeds/01_Users.js
+++ api/seeds/01_Users.js
@@ -1,12 +1,36 @@
 
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(() =>
       // Inserts seed entries
-      knex('table_name').insert([
-        { id: 1, colName: 'rowValue1' },
-        { id: 2, colName: 'rowValue2' },
-        { id: 3, colName: 'rowValue3' },
+      knex('users').insert([
+        {
+          id: 1,
+          login: 'Hernani mattos',
+          email: 'hernani@email.com',
+          password: 123456,
+          name: 'Hernani',
+          guid: 1298361374,
+          create_at: new Date(),
+        },
+        {
+          id: 2,
+          login: 'Juiana Bueno',
+          email: 'juliana@email.com',
+          password: '123456',
+          name: 'Juliana',
+          guid: 1298563274,
+          create_at: new Date(),
+        },
+        {
+          id: 3,
+          login: 'Luca',
+          email: 'luca@email.com',
+          password: 123456,
+          name: 'Luca',
+          guid: 1298564274,
+          create_at: new Date(),
+        },
       ]));
 };
